444c89d6b75f027d5fb66590655aa5d080600967
knowledge-corner <vaidehi.nair@knowledgecorner.in>
2024-08-14 15:00:16 +0530

    adding all components

--- MPG Dash app/app.py
+++ MPG Dash app/app.py
@@ -1,18 +1,29 @@
 from dash import Dash, html, dcc
 import dash_bootstrap_components as dbc
 import dataset as ds
+import plotly.express as px
 
 # Initialize app
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.icons.BOOTSTRAP])
 
 # Build components
 header = html.H2("MPG Analysis Dashboard", className="text-center alert alert-info")
-
-
+bar1 = px.bar(ds.get_pivot_data(), x = "model year", y = "car name", color = "origin", barmode="group", width=800, title="Number of car by model year and country",
+              labels={"car name" : ""})
+hist1 = px.histogram(ds.df, x = "origin", y = "mpg", text_auto=True, color = "origin", width=500, histfunc="avg")
+scatter1 = px.scatter(ds.df, x = "horsepower", y = "mpg", width = 800, facet_col = "origin", trendline="ols")
+pairplot1 = px.scatter_matrix(ds.df, dimensions=['mpg', 'cylinders', 'displacement', 'horsepower', 'weight', 'acceleration'], height=600)
 
 # App Layout
-app.layout = html.Div([
-        dbc.Row(header, class_name="rounded mt-0")
+app.layout = html.Div([ header, html.Hr(),
+                       dcc.Graph(figure=bar1, id="bar1"), html.Br(),
+                       dbc.Row(
+                           [
+                               dbc.Col(dcc.Graph(figure=hist1, id="hist1")),
+                               dbc.Col(dcc.Graph(figure=scatter1, id="scatter1")),
+                           ]
+                       ),
+                       dcc.Graph(figure=pairplot1, id="pairplot1")
 ])
 
 
--- MPG Dash app/dataset.py
+++ MPG Dash app/dataset.py
@@ -5,4 +5,8 @@ df = pd.read_csv(r"auto-mpg.csv")
 df.replace({"?" : 0}, inplace=True)
 df.horsepower = df.horsepower.astype(int)
 df.origin = df.origin.map({1 : "USA", 2 : "Germany", 3 : "Japan"})
-print(df.head())
+
+
+def get_pivot_data():
+    data = df.pivot_table(index = ["origin","model year"], values="car name", aggfunc=lambda x : x.nunique()).round().reset_index()
+    return data
