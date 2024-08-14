c0f34b1c12ec1f4b2aa63e4585715d77fe89cabb
knowledge-corner <vaidehi.nair@knowledgecorner.in>
2024-08-14 14:26:10 +0530

    adding css

--- MPG Dash app/app.py
+++ MPG Dash app/app.py
@@ -3,15 +3,16 @@ import dash_bootstrap_components as dbc
 import dataset as ds
 
 # Initialize app
-app = Dash(__name__)
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.icons.BOOTSTRAP])
 
 # Build components
-header = html.H2("MPG Analysis Dashboard")
+header = html.H2("MPG Analysis Dashboard", className="text-center alert alert-info")
+
 
 
 # App Layout
 app.layout = html.Div([
-        dbc.Row(header)
+        dbc.Row(header, class_name="rounded mt-0")
 ])
 
 
